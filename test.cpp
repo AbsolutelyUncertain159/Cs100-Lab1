dc45248f03e50e9c73fcc13fa0d6d9da4154341f
AbsolutelyUncertain159 <ian91234@gmail.com>
2021-10-05 13:21:31 -0700

    Committing newest version of test.cpp

--- test.cpp
+++ test.cpp
@@ -7,6 +7,41 @@ TEST(EchoTest, HelloWorld) {
     EXPECT_EQ("hello world", echo(3,test_val));
 }
 
+TEST(EchoTest, EmptyString) {
+    char* test_val[1]; test_val[0] = "./c-echo";
+    EXPECT_EQ("", echo(1,test_val));
+}
+
+TEST(EchoTest, SpecialCharacters)
+{
+	char* test_val[3];
+	test_val[0] = "./c-echo";
+	test_val[1] = "!@#$%^&*()";
+	test_val[2] = "%";
+
+	EXPECT_EQ("!@#$%^&*() %", echo(3, test_val));
+}
+
+TEST(EchoTest, NumberTester)
+{
+	char* test_val[2];
+	test_val[0] = "./c-echo";
+	test_val[1] = "115420117";
+
+	EXPECT_EQ("115420117", echo(2, test_val));
+}
+
+TEST(EchoTest, SpacingTester)
+{
+	char* test_val[3];
+	test_val[0] = "./c-echo";
+	test_val[1] = "ta	b ";
+	test_val[2] = " b	at";
+	
+	EXPECT_EQ("ta	b   b	at", echo(3, test_val));
+}
+
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
